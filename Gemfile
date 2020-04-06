b099491831a6b4beb9aba280bdab5db199dd124d
mbabali iryn <mbabaliiryn@gmail.com>
2020-04-06 15:09:16 +0300

    create a scraper for the job search website

--- Gemfile
+++ Gemfile
@@ -1,5 +1,7 @@
-source "https://rubygems.org"
+# frozen_string_literal: true
 
+source 'https://rubygems.org'
+
+gem 'byebug'
 gem 'httparty'
 gem 'nokogiri'
-gem 'byebug'
--- scraper.rb
+++ scraper.rb
@@ -1,13 +1,45 @@
+# frozen_string_literal: true
+
 require 'nokogiri'
 require 'httparty'
 require 'byebug'
 
 def scraper
-    url = "https://www.indeed.com/"
-    unparsed_page = HTTParty.get(url)
-    parsed_page = Nokogiri::HTML(unparsed_page)
-    byebug
+  url = 'https://www.indeed.com/q-Indeed-jobs.html'
+  unparsed_page = HTTParty.get(url)
+  parsed_page = Nokogiri::HTML(unparsed_page)
+  jobs = []
+  job_listings = parsed_page.css('div.listingCard') # 50 jobs
+
+  page = 1
+  per_page = job_listings.count # 50
+  total = parsed_page.css('div.job-count').text
+  last_page = per_page.to_i.round # 46
+
+  while page <= last_page
+    pagination_url = "https://www.indeed.com/q-Indeed-jobs.html/listings?page=#{page}"
+    puts pagination_url
+    puts "page: #{page}"
+    puts ''
+    pagination_unparsed_page = HTTParty.get(pagination_url)
+    pagination_parsed_page = Nokogiri::HTML(pagination_unparsed_page)
+    pagination_job_listings = parsed_page.css('div.listingCard')
+    pagination_job_listings.each do |job_listing|
+      job = {
+        title: job_listing.css('span.job-title').text,
+        company: job_listing.css('span.company').text,
+        location: job_listing.css('span.location').text,
+        url: 'https://www.indeed.com/q-Indeed-jobs.html' + job_listing.css('a')[0].attributes['href'].value
+
+      }
+      jobs << job
+      puts "Added #{job[:title]}"
+      puts ''
+    end
+    page += 1
+
+  end
+  byebug
 end
 
 scraper
-
